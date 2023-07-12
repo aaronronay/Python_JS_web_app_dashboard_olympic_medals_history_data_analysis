050dda52988984564e397f4760bd210859c32c0b
aaronronay <30646034+aaronronay@users.noreply.github.com>
2023-07-11 23:33:51 -0400

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -1,90 +1,87 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask, jsonify, render_template
-import os
-
 import pandas as pd
-import numpy as np
-
 import sqlalchemy
-from sqlalchemy.ext.automap import automap_base
-from sqlalchemy.orm import Session
-from sqlalchemy import create_engine
-import pymysql
-pymysql.install_as_MySQLdb()
 
-#change values to new credentials
+# Change values to new credentials
 database_username = 'root'
 database_password = 'root'
 database_ip = '127.0.0.1:3306'
 database_name = 'olympics'
-database_connection = sqlalchemy.create_engine('mysql+mysqlconnector://{0}:{1}@{2}/{3}'.
-                                               format(database_username, database_password,
-                                                      database_ip, database_name))
+database_connection = f'mysql+mysqlconnector://{database_username}:{database_password}@{database_ip}/{database_name}'
 
-#Flask Initialize
-print('Now starting flask server')
+# Flask Initialize
 app = Flask(__name__)
-#################################################
+
 # Database Setup
-#################################################
-
-app.config["SQLALCHEMY_DATABASE_URI"] = 'mysql+mysqlconnector://{0}:{1}@{2}/{3}'.format(
-    database_username, database_password, database_ip, database_name)
+app.config["SQLALCHEMY_DATABASE_URI"] = database_connection
 db = SQLAlchemy(app)
-
-# reflect an existing database into a new model
-Base = automap_base()
-# reflect the tables
-Base.prepare(db.engine, reflect=True)
+db.Model.metadata.reflect(db.engine)
 
 # Save references to each table
-Master_data = Base.classes.master_data
-####################################################
-##Jsonify to convert data, pd.to_json for dataframes
-####################################################
+Master_data = db.Model.metadata.tables.get('master_data')
 
-print('Now Connecting to MySQL server for query results..')
-mysql_query_df = pd.read_sql(
-    "SELECT * FROM olympics.master_data", con=database_connection)
-print('Now Jsonifying MySql Query..')
-json_data = mysql_query_df.to_json(orient='records')
-
+# Connect to MySQL server for query results
+mysql_query = "SELECT * FROM olympics.master_data"
 
 @app.route("/")
 def index():
     '''Return to the homepage.'''
     return render_template('index.html')
 
-#@app.route("/data/<select1>/<select2>/")
-
-
 @app.route("/data_no-variable/")
 def data():
-    #If you wish to return data that is independent of selection variables,
-    #You can do the processing to calculate the data to return on server instead of client.
-    """Return the json data"""
-    return json_data
+    """Return the JSON data"""
+    try:
+        return jsonify(get_cached_data())
+    except Exception as e:
+        return jsonify({'error': str(e)})
 
-#
 @app.route("/data2/")
 def data2():
-    stmt = db.session.query(Master_data).statement
-    dataframe = pd.read_sql_query(stmt, db.session.bind)
-    """Return the json data"""
-    json_data2 = dataframe.to_json(orient='records')
-    return json_data2
+    """Return the JSON data"""
+    try:
+        return jsonify(get_cached_data2())
+    except Exception as e:
+        return jsonify({'error': str(e)})
 
 @app.route("/summer/<year>")
 def summer_year(year):
-    stmt = db.session.query(Master_data).\
-    filter(Master_data.season == "summer").\
-    filter(Master_data.Year == year).\
-    statement
-    dataframe = pd.read_sql_query(stmt, db.session.bind)
-    #return the dataframe as json
-    json_df = dataframe.to_json(orient='records')
-    return json_df
+    """Return the JSON data"""
+    try:
+        return jsonify(get_cached_summer_year(year))
+    except Exception as e:
+        return jsonify({'error': str(e)})
 
-    
+def get_cached_data():
+    try:
+        if 'cached_data' not in app.config:
+            dataframe = pd.read_sql(mysql_query, con=database_connection)
+            app.config['cached_data'] = dataframe.to_dict(orient='records')
+        return app.config['cached_data']
+    except Exception as e:
+        raise Exception('Error retrieving cached data') from e
+
+def get_cached_data2():
+    try:
+        if 'cached_data2' not in app.config:
+            stmt = db.session.query(Master_data).statement
+            dataframe = pd.read_sql_query(stmt, db.session.bind)
+            app.config['cached_data2'] = dataframe.to_dict(orient='records')
+        return app.config['cached_data2']
+    except Exception as e:
+        raise Exception('Error retrieving cached data 2') from e
+
+def get_cached_summer_year(year):
+    try:
+        cache_key = f"summer_{year}"
+        if cache_key not in app.config:
+            stmt = db.session.query(Master_data).filter(Master_data.c.season == "summer", Master_data.c.Year == year).statement
+            dataframe = pd.read_sql_query(stmt, db.session.bind)
+            app.config[cache_key] = dataframe.to_dict(orient='records')
+        return app.config[cache_key]
+    except Exception as e:
+        raise Exception('Error retrieving cached summer year data') from e
+
 if __name__ == "__main__":
     app.run()
